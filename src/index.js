b0d81073d2c99ffc76076416d36708a67df88d3e
Gabby97 <gabbacvdor@gmail.com>
2019-10-09 17:10:09 -0500

    Subiendo funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-import validator from './validator.js';
+import {isValid} from "./validator.js";
 
-console.log(validator);
+// console.log(validator);
+
+// console.log(lunh('6011724110669057'))
+const $formulario = document.getElementById("formulario");
+const $tarjeta = document.getElementById("num-tarjeta");
+const $respuesta = document.getElementById("respuesta");
+
+// let numeros = [];
+
+$formulario.addEventListener("submit", e => {
+  e.preventDefault();
+  const validTarjeta = $tarjeta.value;
+
+  const resultadoValidacion = isValid(validTarjeta);
+
+  let rutaImagen = '<img src="/img/like.png" alt="like">';
+  if (!resultadoValidacion) {
+    rutaImagen = '<img src="/img/unlike.png" alt="unlike">';
+  }
+
+  $respuesta.innerHTML = rutaImagen //'<img src="/img/like.png" alt="like">';
+  $formulario.reset();
+}, false);
+
+//metodo Reduce
+
+// const letras = ['H', 'O', 'L', 'A'];
+// const palabra = letras.reduce((valor_anterior_retornado, valor_actual) => {
+//   return valor_anterior_retornado + valor_actual;
+// });
+// console.log(palabra);
+
+// //metodo Map
+
+// const misNumeritos = [2, 4, 8, 5];
+// const cuadrados = misNumeritos.map((e) => {
+//   return e + e;
+// });
+// console.log(cuadrados);
+
+// operadores Ternarios
+
+// const hija = 'Gabby Acevedo Rojas';
+// const resultado = (hija === 'Gabby Acevedo Rojas') // referencia a la condicion de un if
+// ? 'Hija de Armando Acevedo y Filomena Rojas' // si la condicion se cumple
+// : 'No es hija de estas hermosas personas'; // else -- si la funcion no se cumple
+
+// console.log(resultado);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,26 @@
-const validator = {
-  // ...
-};
+export const isValid = (creditCardNumber) => {
+    const digitos = creditCardNumber.split('');
+    const pares = digitos.filter((_, indice) => indice % 2 === 0);
+    const impares = digitos.filter((_, indice) => indice % 2 !== 0);
+  
+    const paso1 = pares.map(par => 
+      parseInt(par) * 2 > 9
+      ? 1 + ((parseInt(par)* 2) % 10)
+      : parseInt(par) * 2
+      );
+      const paso2 = paso1.reduce(
+        (acumulador, elemento) => acumulador + elemento, 
+        0
+        );
+       const paso3 = impares.reduce(
+         (acumulador, element) => parseInt(acumulador) + parseInt(element),
+       0
+       );
+       const paso4 = paso2 + paso3;
+       const paso5 = paso4 % 10;
+  
+       const esValido = paso5 === 0;
+      return esValido;
+  };
 
-export default validator;
+  // ...
