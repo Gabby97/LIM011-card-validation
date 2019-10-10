ac0a187f8401148ccf54a99433c3df94d4671fc8
Gabby97 <gabbacvdor@gmail.com>
2019-10-10 16:00:41 -0500

    testeo

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -1,25 +1,26 @@
 // importamos el objeto `validator`, que contiene la función `isValid`
-import validator from "../src/validator";
+import {isValid} from "../src/validator";
 
-describe('validator', () => {
 
-  it('debería ser un objeto', () => {
-    expect(typeof validator).toBe('object');
-  });
+  /* it('debería ser un objeto', () => {
+    expect(typeof isValid).toBe('object');
+  }); */
 
   describe('validator.isValid', () => {
 
     it('debería ser una función', () => {
-      expect(typeof validator.isValid).toBe('function');
+      expect(typeof isValid).toBe('function');
     });
 
-    it.skip('debería retornar true para "4083952015263"', () => {
+    it('debería retornar true para "4079076706388905"', () => {
       // escribe aquí tu test
+      expect(isValid("4079076706388905")).toBe(true);
     });
 
-    it.skip('debería retornar false para "1234567890"', () => {
+    it('debería retornar false para "1234567890"', () => {
       // escribe aquí tu test
+      expect(isValid("1234567890")).toBe(false);
+
     });
 
   });
-});
